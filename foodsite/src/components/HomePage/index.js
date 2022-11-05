be6e92e956a15f5506e3c41018476d81d02d9692
AhmadOthman1 <103585755+AhmadOthman1@users.noreply.github.com>
2022-11-05 14:51:50 +0200

    home section

--- foodsite/src/components/HomePage/index.js
+++ foodsite/src/components/HomePage/index.js
@@ -3,6 +3,7 @@ import Style from "./style.module.css";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import searchIcon from '../../images/search.png'
 function HomePage() {
   return (
     <>
@@ -12,16 +13,17 @@ function HomePage() {
             <h1>Order Healthy And Fresh Food Any Time</h1>
             <p>Italian food makes people think of big family dinners. So you may want to position your restaurant as a place to bring the whole family.</p>
             <div className={Style.homeSearch}>
-              <Row>
+              <Row className={Style.searchRow}>
                 <Col xs={9} >
-                <h3>Search Keywords</h3>
-                <input type="text" value="Recipe Name Here!"/>
+                <h5>Search Keywords</h5>
+                <input type="text" placeholder="Recipe Name Here!"/>
                 </Col>
                 <Col  xs={2}>
-                <Button variant="warning">Warning</Button>
+                <Button className={Style.searchButton} variant="warning"><img src={searchIcon}/></Button>
                 </Col>
               </Row>
             </div>
+            <h3>Popular Restaurant</h3>
           </Col>
           <Col xs={5} ></Col>
         </Row>
