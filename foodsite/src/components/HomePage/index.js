e9180e05e4c9535264d1ed3cce4d64021752e989
AhmadOthman1 <103585755+AhmadOthman1@users.noreply.github.com>
2022-11-05 14:32:40 +0200

    home section

--- foodsite/src/components/HomePage/index.js
+++ foodsite/src/components/HomePage/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Style from "./style.module.css";
-import Container from 'react-bootstrap/container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
 function HomePage() {
   return (
-    <div className={Style.HomeSection}>
-
-    </div>
+    <>
+      <div className={Style.HomeSection}>
+        <Row className={Style.HomeMain}>
+          <Col xs={6} >
+            <h1>Order Healthy And Fresh Food Any Time</h1>
+            <p>Italian food makes people think of big family dinners. So you may want to position your restaurant as a place to bring the whole family.</p>
+            <div className={Style.homeSearch}>
+              <Row>
+                <Col xs={9} >
+                <h3>Search Keywords</h3>
+                <input type="text" value="Recipe Name Here!"/>
+                </Col>
+                <Col  xs={2}>
+                <Button variant="warning">Warning</Button>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+          <Col xs={5} ></Col>
+        </Row>
+      </div>
+    </>
   )
 }
 
